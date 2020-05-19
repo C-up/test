60872b21d9f357e2b9102d00864b2116365212d7
offcn-java <780278667@qq.com>
2020-05-19 22:41:28 +0800

    sa

---
+++ src/main/java/com/demo/tq.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+/**
+ * @program: Demo2
+ * @description:
+ * @author: Mr.C
+ * @create: 2020-05-19 22:39
+ **/
+public class tq {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
