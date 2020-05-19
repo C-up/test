146058ccc68a87c4d6904aafc9b66edd5027dcfe
offcn-java <780278667@qq.com>
2020-05-19 22:56:42 +0800

    分支提交的

---
+++ src/main/java/com/demo/te.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+/**
+ * @program: Demo2
+ * @description:
+ * @author: Mr.C
+ * @create: 2020-05-19 22:56
+ **/
+public class te {
+    public static void main(String[] args) {
+        System.out.println(234);
+    }
+
+}
